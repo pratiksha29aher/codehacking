b32301b4e6e4ee7fc86e20be6dc9a9132c3e9bc1
Pratiksha <pratiksha29aher@gmail.com>
2018-09-26 16:05:23 +0530

    Users with Photos are working

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,8 +16,8 @@
             <a class="navbar-brand" href="#">WebSiteName</a>
         </div>
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Page 1</a></li>
+            <li class="active"><a href="{{route('users.index')}}">All User</a></li>
+            <li><a href="{{route('users.create')}}">Add User</a></li>
             <li><a href="#">Page 2</a></li>
             <li><a href="#">Page 3</a></li>
         </ul>
